d9d3a0f4570715a7e31891d3603d2bf0029bf040
Nerdeee <48033320+Nerdeee@users.noreply.github.com>
2023-12-18 12:47:19 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "openGL Tutorial", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Could not create window" << std::endl;
 		
@@ -24,30 +24,27 @@ int main() {
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize glad" << std::endl;
-		glfwTerminate();
+		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	
-	//render loop
-	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
 
+	while (!glfwWindowShouldClose(window)) {
+		//process this input
+		processInput(window);
 		//render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
 		//send new frame to window
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
 
 	glfwTerminate();
-	return  0;
+	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {

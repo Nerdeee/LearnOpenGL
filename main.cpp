fc0a912aa7eaf40a61d994d0038129b662a403e8
Nerdeee <48033320+Nerdeee@users.noreply.github.com>
2023-12-21 21:13:14 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -68,11 +68,29 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f
+	float vertices[] = {				// crown - not sure why this is 
+		-0.5f, -0.5f, 0.0f,				// different from the rectangle
+		0.5f, 0.5f, 0.0f,				// will have to look into further
+		0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
 	};
+
+	unsigned int indices[] = {
+		1, 2, 0,	// first triangle
+		2, 0, 3		// second triangle
+	};
+
+	/*float vertices[] = {					// rectangle
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};*/
 
 	/*
 	
@@ -134,6 +152,13 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// EBO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -152,7 +177,8 @@ int main() {
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 		//send new frame to window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
